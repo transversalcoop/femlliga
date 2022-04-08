28647c25a22cb647b8f207c2f89d61989f733c03
Oriol Flors Mas <oriol.flors.mas@gmail.com>
2022-04-08 09:03:17 +0200

    Tested the complete happy path of the application. Fixed some minor template issues along the way.

--- femlliga/tests.py
+++ femlliga/tests.py
@@ -8,11 +8,19 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.exceptions import PermissionDenied
 
+from allauth.account.models import EmailAddress
+
 from .models import *
 from .constants import *
 from .utils import date_intervals
 
 AUTH_BACKENDS = settings.AUTHENTICATION_BACKENDS[1:]
+PASS_FOR_TESTS = "passfortests"
+
+# helper for visualizing response page when test fails
+def save_response(response):
+    with open('/tmp/tmp.html', 'w') as f:
+        f.write(str(response.content.decode("utf-8")))
 
 class ResourcesTests(TestCase):
     def test_resource_types_match(self):
@@ -99,7 +107,7 @@ class SmokeTests(TestCase):
 
     @override_settings(AUTHENTICATION_BACKENDS = AUTH_BACKENDS)
     def test_logged(self):
-        self.client.login(email='test@example.com', password='passfortests')
+        self.client.login(email="test@example.com", password=PASS_FOR_TESTS)
         org, org2 = self.get_orgs()
         URLS = [
             ("app", "Example organization", []),
@@ -114,7 +122,7 @@ class SmokeTests(TestCase):
 
     @override_settings(AUTHENTICATION_BACKENDS = AUTH_BACKENDS)
     def test_logged_error(self):
-        self.client.login(email='test@example.com', password='passfortests')
+        self.client.login(email="test@example.com", password=PASS_FOR_TESTS)
         org, org2 = self.get_orgs()
         self.aux_get("edit_organization", args=[org2.id], status_code=403)
 
@@ -129,8 +137,8 @@ class SmokeTests(TestCase):
                 self.assertContains(response, s, msg_prefix=url)
 
     def get_orgs(self):
-        org = get_user_model().objects.get(email='test@example.com').organizations.all()[0]
-        org2 = get_user_model().objects.get(email='test2@example.com').organizations.all()[0]
+        org = get_user_model().objects.get(email="test@example.com").organizations.all()[0]
+        org2 = get_user_model().objects.get(email="test2@example.com").organizations.all()[0]
         return org, org2
 
 class ComponentTests(TestCase):
@@ -153,6 +161,179 @@ class ComponentTests(TestCase):
 class IntegrationTests(TestCase):
     fixtures = ["testdata.json"]
 
-    # TODO test the full interaction of a new user
+    def aux_wizard(self, url, resource, options, comments, has_resource, contains, charge=False):
+        params = {
+            "resource": resource,
+            "options": options,
+            "comments": comments,
+            "has_resource": has_resource,
+        }
+        if charge:
+            params["charge"] = "on"
+        response = self.client.post(url, params, follow=True)
+        self.assertContains(response, contains)
+
+    @override_settings(AUTHENTICATION_BACKENDS = AUTH_BACKENDS)
     def test_happy_path(self):
-        pass
+        # signup
+        response = self.client.post(reverse("account_signup"), {
+            "email": "test3@example.com",
+            "password1": PASS_FOR_TESTS,
+            "password2": PASS_FOR_TESTS,
+        }, follow=True)
+        self.assertContains(response, "Verifica el correu electrònic")
+        e = EmailAddress.objects.get(email="test3@example.com")
+        e.verified = True
+        e.save()
+        self.client.login(email="test3@example.com", password=PASS_FOR_TESTS)
+
+        # add organization
+        response = self.client.get(reverse("app"), follow=True)
+        self.assertContains(response, "Sembla que encara no heu donat d'alta cap entitat")
+
+        response = self.client.post(reverse("add_organization"), {
+            "name": "Nom entitat de test",
+            "description": "Descripció entitat de test",
+            "social_media-0-media_type": "INSTAGRAM",
+            "social_media-0-value": "usuari_instagram_de_test",
+            "social_media-TOTAL_FORMS": "6",
+            "social_media-INITIAL_FORMS": "0",
+            "org_type": "ASSOCIATION",
+            "scopes": ["EQUALITY", "EDUCATION"],
+            "lat": "40.000",
+            "lng": "1.000",
+        }, follow=True)
+        self.assertContains(response, "preguntes sobre els recursos que teniu o necessiteu")
+        o = Organization.objects.get(name="Nom entitat de test")
+
+        # needs wizard
+        response = self.client.post(reverse("pre-wizard", args=[o.id]), {"start": "yes"}, follow=True)
+        self.assertContains(response, "Esteu buscant un local on poder desenvolupar")
+
+        needs_url = reverse("resources-wizard", args=[o.id, "needs"])
+        self.aux_wizard(needs_url, "PLACE", ["DAILY_USAGE", "PUNCTUAL_USAGE"], "comentaris de necessita local de test", "yes",
+            "Esteu buscant algú que us proporcione aquests serveis")
+
+        self.aux_wizard(needs_url, "SERVICE", ["AGENCY"], "comentaris de necessita servei de test", "yes",
+            "Voleu rebre formació en algun d&#39;aquests temes")
+
+        self.aux_wizard(needs_url, "TRAINING", ["TRAINING_DIGITAL"], "comentaris de necessita formació de test", "yes",
+            "Necessiteu alguna d&#39;aquestes coses")
+
+        self.aux_wizard(needs_url, "EQUIPMENT", [], "comentaris de necessita material de test", "no",
+            "Podeu indicar qualsevol altra necessitat que tingueu")
+
+        self.aux_wizard(needs_url, "OTHER", [], "comentaris de necessita altres de test", "no",
+            "Ja vas per la meitat")
+
+        # offers wizard
+        response = self.client.post(reverse("mid-wizard", args=[o.id]), {"start": "yes"}, follow=True)
+        self.assertContains(response, "Teniu un local que estigueu disposats a compartir amb altres entitats")
+
+        offers_url = reverse("resources-wizard", args=[o.id, "offers"])
+        self.aux_wizard(offers_url, "PLACE", [], "comentaris de ofereix local de test", "no",
+            "Oferiu algun d&#39;aquests serveis per a altres entitats")
+
+        self.aux_wizard(offers_url, "SERVICE", [], "comentaris de ofereix serveis de test", "no",
+            "Oferiu formació en algun d&#39;aquests temes")
+
+        self.aux_wizard(offers_url, "TRAINING", ["TRAINING_DIGITAL"], "comentaris de ofereix formació de test", "yes",
+            "Teniu alguna d&#39;aquestes coses que pugueu compartir", charge = True)
+
+        self.aux_wizard(offers_url, "EQUIPMENT", [], "comentaris de ofereix material de test", "yes",
+            "Podeu indicar qualsevol altre servei o material que oferiu", charge = True)
+
+        self.aux_wizard(offers_url, "OTHER", [], "comentaris de ofereix altres de test", "yes",
+            "Has acabat d'introduir la informació de la teua associació", charge = True)
+
+        # app main page
+        response = self.client.post(reverse("post-wizard", args=[o.id]), {"start": "yes"}, follow=True)
+        should_contain = [
+            "Nom entitat de test",
+            "Descripció entitat de test",
+            "Instagram",
+            "usuari_instagram_de_test",
+            "Associació",
+            "Dona/Igualtat/Feminismes",
+            "Local",
+            "Servei",
+            "Formació",
+            "Equipaments",
+            "Altres",
+            "comentaris de necessita local de test",
+            "comentaris de necessita servei de test",
+            "comentaris de necessita formació de test",
+            "comentaris de ofereix formació de test",
+            "comentaris de ofereix material de test",
+            "comentaris de ofereix altres de test",
+            "Es demana remuneració a canvi",
+        ]
+        for s in should_contain:
+            self.assertContains(response, s)
+        for s in [
+            "comentaris de necessita material de test",
+            "comentaris de necessita altres de test",
+            "comentaris de ofereix local de test",
+            "comentaris de ofereix serveis de test",
+        ]:
+            self.assertNotContains(response, s)
+
+        # matches page
+        response = self.client.get(reverse("matches", args=[o.id]))
+        for s in [
+            "Hem trobat coincidències entre les vostres necessitats",
+            "Local",
+            "Posa't en contacte amb Second example organization",
+        ]:
+            self.assertContains(response, s)
+
+        for s in ["Servei", "Formació", "Equipaments", "Altres"]:
+            self.assertNotContains(response, s)
+
+        # view organization page
+        response = self.client.get(reverse("view_organization", args=[o.id]))
+        for s in should_contain[:6]:
+            self.assertContains(response, s)
+
+        # send message
+        o2 = Organization.objects.get(name="Second example organization")
+        send_message_url = reverse("send_message", args=[o.id, o2.id, "offer", "PLACE"])
+        response = self.client.get(send_message_url)
+        for s in ["que podrà acceptar o rebutjar la comunicació", "Detalls del que s'està sol·licitant"]:
+            self.assertContains(response, s)
+
+        response = self.client.post(send_message_url, {
+            "options": ["DAILY_USAGE", "PUNCTUAL_USAGE"],
+            "message": "missatge de test per al primer missatge",
+        }, follow=True)
+        self.assertContains(response, "S'ha enviat el missatge correctament.")
+        self.assertEqual(len(mail.outbox), 1)
+
+        # check received
+        self.client.login(email="test2@example.com", password=PASS_FOR_TESTS)
+        response = self.client.get(reverse("agreements_received", args=[o2.id]))
+        for s in ["Peticions de col·laboració rebudes", "ens sol·licita Local"]:
+            self.assertContains(response, s)
+
+        a = Agreement.objects.get(solicitor=o, solicitee=o2)
+        response = self.client.post(reverse("agreement_connect", args=[o2.id, a.id]), {
+            "return_url": "received",
+            "connect": "yes",
+        }, follow=True)
+        save_response(response)
+        self.assertContains(response, "S&#39;ha iniciat la comunicació")
+        self.assertEqual(len(mail.outbox), 2)
+
+        # check sent
+        self.client.login(email="test3@example.com", password=PASS_FOR_TESTS)
+        response = self.client.get(reverse("agreements_sent", args=[o.id]))
+        for s in ["Peticions de col·laboració enviades", "Local sol·licitat a", "va acceptar la comunicació"]:
+            self.assertContains(response, s)
+
+        response = self.client.post(reverse("agreement_successful", args=[o.id, a.id]), {
+            "return_url": "sent",
+            "successful": "yes",
+        }, follow=True)
+        self.assertContains(response, "Has indicat que s&#39;ha arribat a un acord")
+        self.assertEqual(len(mail.outbox), 2)
+
